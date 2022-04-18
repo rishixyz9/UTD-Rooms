e76b6364c675217f954b1780e9e30ae27115be8c
Raghav <raghavpillai101@gmail.com>
2022-04-18 14:32:50 -0500

    Updated gitignore and test input

--- index.js
+++ index.js
@@ -6,27 +6,22 @@
 
 // Master control of indexing and searching processes
 
-require("./classes/exports.js")(); // Require all exported functions
+require("./modules/exports.js")(); // Require all exported functions
 
 ingest(); // Start ingest
 
 // Debugging
 //checkBuildings();
-//console.log(buildings[0].floors[0].classes);
-
-// Input
 
 // Test inputs
-let day = "Friday";
-let building = "SCI";
-let floor = "3";
+let day = "Thursday";
+let building = "ECSN";
+let floor = "2";
 let time_start = "15:00";
-let time_end = "19:00";
+let time_end = "16:00";
 
 // Build inputs
 //let day, building, floor, time_start, time_end = input();
 
 // Search based on inputs
 search(day, building, floor, time_start, time_end);
-
-//checkBuildings();
